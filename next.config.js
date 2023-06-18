2e0d951ba4916ef719914a6085be3c818b22143b
dongyeopCha <ckehdduq95@gmail.com>
2023-06-18 21:58:30 +0900

    chore: eslint prettier setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    instrumentationHook: true,
+    serverActions: true,
+  },
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
