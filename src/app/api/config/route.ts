29069f15f8364b26f1fd40aabbfefefb6946816e
dongyeopCha <ckehdduq95@gmail.com>
2023-06-23 12:29:06 +0900

    fix: 코드 리뷰사항 반영

--- src/app/api/config/route.ts
+++ src/app/api/config/route.ts
@@ -2,10 +2,10 @@ import User from '@/lib/models/User';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  const key = new URL(req.url).searchParams.get('key');
+  const key = req.nextUrl.searchParams.get('key');
   await User.updateOne({ uuid: key }, { isAuthenticated: true });
   const response = NextResponse.json(
-    { message: '인증이 완료되었습니다.서비스로 이동해주세요 :)' },
+    { message: '인증이 완료되었습니다. 서비스로 이동해주세요 :)' },
     {
       status: 200,
     }
